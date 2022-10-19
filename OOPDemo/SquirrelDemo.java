6eaf4c98d349c44c7bf75915be8ccde96240b46f
kyliesinc <kylie.sinclair23@ycdsbk12.ca>
2022-10-19 09:42:38 -0400

    squirrel demo

--- OOPDemo/SquirrelDemo.java
+++ OOPDemo/SquirrelDemo.java
@@ -1,5 +1,29 @@
 package OOPDemo;
+import OOPDemo.Squirrel;
 
 public class SquirrelDemo {
     
+    public static void main(String[] args) {
+
+        // define variables
+        Squirrel waltSquirrel;
+        Squirrel jesseSquirrel;
+        Squirrel beanSquirrel;
+        Squirrel alvinSquirrel;
+
+        // set variables for the specific squirrels
+        waltSquirrel = new Squirrel(20.3, 10.5, "Brown", "Brown", "Walter");
+        jesseSquirrel = new Squirrel(25.1, 2.6, "Gray", "Western Gray Squirrel", "Jesse");
+        beanSquirrel = new Squirrel(43.8, 15.1, "Black", "Black Squirrel", "Bean");
+        alvinSquirrel = new Squirrel(12.3, 5.2, "Brown", "Chipmunk", "Alvin");
+
+        // get them to do stuff by calling methods from squirrel main
+        waltSquirrel.walk();
+        jesseSquirrel.storeNuts();
+        beanSquirrel.scramble();
+        alvinSquirrel.injury();
+
+        System.out.println("Alvin's Health: " + alvinSquirrel.getHealth());
+
+    }
 }
